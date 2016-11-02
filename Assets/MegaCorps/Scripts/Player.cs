ef59feb969008f54514f62ac1d30e0b4aa3851be
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2016-11-02 23:04:33 +0200

    Initial commit.
    Basic city map.
    Turn based with 4 players.
    Income from city blocks at end of round.

---
+++ Assets/MegaCorps/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public string name = "Iceman";
+    public Color color = Color.red;
+    public int cash = 50000;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
